3f7ba1c5de4367c4e2880b8450d638931b71fe82
nicks52 <1nicks52@gmail.com>
2017-03-23 12:27:42 +0530

    Adding another resolution param

--- screen_size/features/homepage_tc.feature
+++ screen_size/features/homepage_tc.feature
@@ -4,3 +4,9 @@ Scenario:Checking for resolution for 1024x726(Desktop)
 Given I am on housing.com home page
 When I resize the window
 Then Page is adjusting as per resolution
+
+@500x300
+Scenario:Checking for resolution for 500x300(Desktop)
+Given I am on housing.com home page
+When I again resize the window
+Then Page is adjusting as per resolution
--- screen_size/features/step_definitions/resolution_steps.rb
+++ screen_size/features/step_definitions/resolution_steps.rb
@@ -1,3 +1,4 @@
+
 Given(/^I am on housing\.com home page$/) do
   @b = Selenium::WebDriver.for :firefox
   @b.navigate.to "https://housing.com/in/buy/real-estate-pune"
@@ -10,6 +11,15 @@ When(/^I resize the window$/) do
 
   # set window size using Dimension structure
     target_resolution = Selenium::WebDriver::Dimension.new(1024, 600)
+    @b.manage.window.size = target_resolution
+    puts @b.manage.window.size
+end
+
+
+
+When (/^I again resize the window$/) do
+  # set window size using Dimension structure
+    target_resolution = Selenium::WebDriver::Dimension.new(500, 300)
     @b.manage.window.size = target_resolution
     puts @b.manage.window.size
 end
